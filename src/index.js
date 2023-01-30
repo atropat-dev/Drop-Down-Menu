19349d62f5d1ebcc49583aec81e25386e6f25f55
Your Name <hosseinmohammadidev@gmail.com>
2023-01-30 17:48:44 +0330

    sdfdl lkdsjfl

--- src/index.js
+++ src/index.js
@@ -3,5 +3,11 @@ import ReactDOM from "react-dom/client"
 import "./index.css"
 import App from "./App"
 
-const root = ReactDOM.createRoot(document.getElementById("root"))
+function god(){
+  alert("my god is koskhol")
+}
+<button onClick={god}>kir</button>
+
+
+const root = ReactDOM.createRoot(document.getElementById("root")) 
 root.render(<App />)
