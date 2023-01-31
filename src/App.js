575c5bbb79a60d62bc6159c379742d13f12e0558
Your Name <hosseinmohammadidev@gmail.com>
2023-01-31 17:13:26 +0330

    Created styles

--- src/App.js
+++ src/App.js
@@ -1,24 +1,17 @@
-import React from "react";
+import React from "react"
+import Navbar from "./Navbar"
+import NavItem from "./NavItem"
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Reactsd
-        </a>
-      </header>
+      <Navbar>
+        <NavItem icon="😁"/>
+        <NavItem icon="😁"/>
+        <NavItem icon="😁"/>
+      </Navbar>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
---
+++ src/NavItem.jsx
@@ -0,0 +1,13 @@
+import React from "react"
+
+function NavItem(props) {
+  return (
+    <li className="nav-item">
+      <a href="#" className="icon-button">
+        {props.icon}
+      </a>
+    </li>
+  )
+}
+
+export default NavItem
